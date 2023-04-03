b13dc7d3761810c29b374b3881344cd0b2e0e1b1
Kıvanç Yıldız <127532036+Kvncyldz33@users.noreply.github.com>
2023-04-03 17:24:59 +0300

    Update sinema salonu.cpp

--- sinema salonu.cpp
+++ sinema salonu.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
   int yas;
   cout << "Lutfen yasinizi girin: ";
   cin >> yas;
 
-  if (yas < 18) {
+  if (yas < 18)
+  {
     cout << "Bilet ucreti: 15 TL." << endl;
-  } else if (yas >= 65) {
+  }
+  else if (yas >= 65)
+  {
     cout << "Bilet ucreti: 10 TL." << endl;
-  } else {
+  }
+  else
+  {
     cout << "Bilet ucreti: 20 TL." << endl;
   }
-
   return 0;
 }
