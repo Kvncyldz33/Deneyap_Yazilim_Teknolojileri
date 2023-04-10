a4a8ec628c99e419e5e7aa380745183e70f5895c
Kıvanç Yıldız <127532036+Kvncyldz33@users.noreply.github.com>
2023-04-10 17:30:47 +0300

    Create döngüler.cpp
    Döngü Çeşitleri

---
+++ döngüler.cpp
@@ -0,0 +1,112 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    for(int sayi=0;sayi<10;sayi++) // 0 1 2 3 4 5 6 7 8 9 rakamlarýný alt alta yazar.
+ // for(int sayi=10;sayi>=0;sayi--) // 10 9 8 7 6 5 4 3 2 1 0 rakamlarýný alt alta yazar.
+ // for(int sayi=10;sayi>=0;sayi-=2) // 10 8 6 4 2 0 rakamlarýný alt alta yazar.
+ // for(int i=15;i>=0;i-=3) // 15 12 9 6 3 0 rakamlarýný alt alta yazar.
+       cout << sayi << endl;
+    return 0;
+}
+*/
+
+
+/*
+// Alt alta 10 tane deneyap yazar
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<10;i++)
+        cout << "DENEYAP" << endl;
+    return 0;
+}
+*/
+
+
+//////////////
+/*
+// 1'den 99'a kadar olan rakamlarýný alt alta yazar.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sayi = 1;
+    while(sayi<100)
+        {
+            cout << sayi <<endl;
+            sayi ++;
+        }
+return(0);
+}
+*/
+
+//////////////
+
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=1; i<10; i++) // 1 2 3 4 5 6 7 8 9 rakamlarýný alt alta yazar.
+        {
+            cout << i <<endl;
+        }
+    return (0);
+}
+*/
+
+//////////////
+
+/*
+// FOR
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+for(i=1;i<20;i++)
+    if(i%2==1)
+    cout << i << endl; // 1 3 5 7 9 11 13 15 17 19 rakamlarýný alt alta yazar.
+return (0);
+}
+*/
+//////////////
+
+/*
+// WHILE
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<20)
+        {
+            if(i%2==1)
+            cout << i << endl; // 1 3 5 7 9 11 13 15 17 19 rakamlarýný alt alta yazar.
+            i++;
+        }
+return (0);
+}
+*/
+
+//////////////
+
+/*
+// DO WHILE
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    do
+        {
+            if(i%2==1)
+            cout << i << endl; // 1 3 5 7 9 11 13 15 17 19 rakamlarýný alt alta yazar.
+            i++;
+        }while(i<20);
+return (0);
+}
+*/
+
