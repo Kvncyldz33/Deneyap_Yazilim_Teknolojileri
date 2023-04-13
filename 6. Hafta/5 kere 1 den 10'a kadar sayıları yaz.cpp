962c493b0a9c4813a1beda0f285d791974cb7091
Kıvanç Yıldız <127532036+Kvncyldz33@users.noreply.github.com>
2023-04-13 08:57:51 +0300

    Create 5 kere 1 den 10'a kadar sayıları yaz.cpp

---
+++ 6. Hafta/5 kere 1 den 10'a kadar sayıları yaz.cpp
@@ -0,0 +1,17 @@
+// iç içe for örneği  
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j;
+for (i = 1;i<=5;i++)
+{
+    for (j = 1;j<=10;j++)
+    {
+        cout << i << " - " << j << endl;
+    }
+    cout << endl;
+}
+return 0;
+}
